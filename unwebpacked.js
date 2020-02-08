845a7099fa6d898817254a8db63861d7c629ae0d
arthurbernierjr <ma2bernier@gmail.com>
2020-02-08 05:11:23 -0500

    Fixing issue with webpack

---
+++ unwebpacked.js
@@ -0,0 +1,22 @@
+import  {   Komponent,
+  render,
+  patch,
+  ko,
+  fu
+}
+from './lib/kofu-framework/index.js'
+
+import {
+  createElement
+} from "./lib/kofu-framework/element.js";
+
+global.h = createElement;
+const h = createElement;
+export {
+  Komponent,
+  render,
+  patch,
+  ko,
+  fu,
+  h
+};
